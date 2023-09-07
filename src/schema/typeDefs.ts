f49b9a05e5e12146473a7a6bbaac6652e6d4afe9
edahmitchel <edahmitchel@gmail.com>
2023-09-07 11:24:04 +0100

    made v4 changes

--- src/schema/typeDefs.ts
+++ src/schema/typeDefs.ts
@@ -2,47 +2,88 @@
 // that together define the "shape" of queries that are executed against
 // your data.
 
+// export const typeDefs = `#graphql
+//   # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+//   # This "Book" type defines the queryable fields for every book in our data source.
+//   type Book {
+//     title: String
+//     author: String
+//   }
+
+//   # The "Query" type is special: it lists all of the available queries that
+//   # clients can execute, along with the return type for each. In this
+//   # case, the "books" query returns an array of zero or more Books (defined above).
+//   type Query {
+//     books: [Book]
+//   }
+
+// #   my data 
+// type User {
+//     _id: ID!
+//     email: String!
+//     password: String!
+//     username: String!
+
+// }
+// type AuthData {
+//     user:User!
+//     token:String!
+// }
+// input UserInputData {
+//     email: String!
+//     username: String!
+//     pass: String!
+// }
+
+
+// type RootQuery {
+//     login(email:String!,pass:String!):AuthData!
+// }
+
+// type RootMutation{
+//     createUser(userInput:UserInputData):User!
+// }
+
+// `;
 export const typeDefs = `#graphql
-  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+# Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
 
-  # This "Book" type defines the queryable fields for every book in our data source.
-  type Book {
-    title: String
-    author: String
-  }
-
-  # The "Query" type is special: it lists all of the available queries that
-  # clients can execute, along with the return type for each. In this
-  # case, the "books" query returns an array of zero or more Books (defined above).
-  type Query {
-    books: [Book]
-  }
-
-#   my data 
+# This "Book" type defines the queryable fields for every book in our data source.
+type Book {
+  title: String
+  author: String
+}
 type User {
-    _id: ID!
-    email: String!
-    password: String!
-    username: String!
-    
+  _id: ID!
+  email: String!
+  password: String!
+  username: String!
+
 }
 type AuthData {
-    user:User!
-    token:String!
+  user:User!
+  token:String!
 }
 input UserInputData {
-    email: String!
-    username: String!
-    pass: String!
+  email: String!
+  username: String!
+  pass: String!
 }
 
- 
-type RootQuery {
-    login(email:String!,pass:String!):AuthData!
+
+type Query {
+  login(email:String!,pass:String!):AuthData!
 }
 
-type RootMutation{
-    createUser(userInput:UserInputData):User!
+type Mutation{
+  createUser(userInput:UserInputData):User!
 }
 
+# The "Query" type is special: it lists all of the available queries that
+# clients can execute, along with the return type for each. In this
+# case, the "books" query returns an array of zero or more Books (defined above).
+type Query {
+  books: [Book]
+}
 `;
